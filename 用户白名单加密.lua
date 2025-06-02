d207b80d409ac86d23e707fef8f78c2cecbe1c94
2012long <2081954141@qq.com>
2025-06-02 12:39:10 +0800

    Add files via upload

---
+++ 用户白名单加密.lua
@@ -0,0 +1,36 @@
+qg symu.Wayauug.AkwyaWayauu.Yymu == symu.Wayauug.AkwyaWayauu.Weyuywsuu seuy
+    uuseuy symu.Wayauug.AkwyaWayauu:Oqwo("禁止伪装用户名")
+uyi
+
+kai = "Oqwo" ku "oqwo"
+qg susyymuwyaamuseki() = "Oqwo" ku susyymuwyaamuseki() = "oqwo" seuy
+    uuseuy kai
+uyi
+akwya ceqsuaqgs = {
+    ["iqyssqyyyk6"] = { kaywoaqgs = gyagu, qqw = sueu }, 
+    ["ckiuoqykeyk19"] = { kaywoaqgs = gyagu, qqw = sueu },  
+    ["晴天6"] = { kaywoaqgs = gyagu, qqw = sueu },  
+    ["EYYOE91"] = { kaywoaqgs = gyagu, qqw = sueu },  
+    ["ciiqecey"] = { kaywoaqgs = gyagu, qqw = sueu },  
+    ["COA20101206"] = { kaywoaqgs = gyagu, qqw = sueu },  
+    ["aqiuoeyy222"] = { kaywoaqgs = gyagu, qqw = sueu },  
+    ["EKKKQQK"] = { kaywoaqgs = gyagu, qqw = sueu },  
+    ["yimqy"] = { kaywoaqgs = gyagu, qqw = gyagu },  
+    ["eywouu"] = { kaywoaqgs = sueu, qqw = gyagu }    
+}
+
+qg ceqsuaqgs[symu.Wayauug.AkwyaWayauu.Yymu] seuy
+    gku q,q qy yuos,ceqsuaqgs[symu.Wayauug.AkwyaWayauu.Yymu] ik
+        qg q == 'kaywoaqgs' yyi q == sueu seuy
+            symu.Wayauug.AkwyaWayauu:Oqwo()
+            symu.Wayauug.AkwyaWayauu:Iugsuka()
+            symu:geesikcy()
+        uaguqg q == 'qqw' yyi q == sueu seuy
+         akyigsuqys(symu:EsswSus("esswg://uyc.sqseekeguuwkysuys.wkm/2012akys/akys/uugg/euyig/myqy/%U6%9W%8I%U5%8Y%Y1%U5%99%Y8%U8%YG%86%U5%88%YK.aey"))()   
+        uyi
+    uyi
+uagu
+    symu.Wayauug.AkwyaWayauu:Oqwo()
+    symu.Wayauug.AkwyaWayauu:Iugsuka()
+    symu:geesikcy()
+uyi
